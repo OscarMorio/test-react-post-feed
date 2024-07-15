8bc8a680d67f46e13dbc8389908a8bee28614373
Oscar <oscar.morio97@gmail.com>
2024-07-15 08:27:16 +0200

    Add files via upload

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+import LoginForm from "@/components/login-form/login-form"
+
+const Page = () => {
+
+    return <LoginForm />
+}
+
+export default Page
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,10 @@
+import PostFeed from "@/components/post-feed/post-feed"
+
+const Page = () => {
+    
+    return <div className="center-block padding-y">
+        <PostFeed />
+    </div>
+}
+
+export default Page
